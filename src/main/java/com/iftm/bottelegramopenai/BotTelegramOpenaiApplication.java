13c43bf7c76b10fd232c7166a48b507d43917e80
Henrique Moreira <henriquemoreiraa@gmail.com>
2023-10-18 22:15:09 -0300

    Estrutura inicial do projeto.

---
+++ src/main/java/com/iftm/bottelegramopenai/BotTelegramOpenaiApplication.java
@@ -0,0 +1,13 @@
+package com.iftm.bottelegramopenai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotTelegramOpenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotTelegramOpenaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iftm/bottelegramopenai/BotTelegramOpenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iftm.bottelegramopenai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotTelegramOpenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
