28be419bd44333a18fe6d7d2f030786359d0ca0c
Henrique Moreira <henriquemoreiraa@gmail.com>
2023-10-18 22:28:57 -0300

    Implementação final.

--- src/main/java/com/iftm/bottelegramopenai/service/TelegramBotService.java
+++ src/main/java/com/iftm/bottelegramopenai/service/TelegramBotService.java
@@ -8,9 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import com.iftm.bottelegramopenai.dto.ChatGPTResponse.Choice;
+import com.iftm.bottelegramopenai.dto.ChatGPTResponse;
 import com.iftm.bottelegramopenai.dto.Message;
 
 import lombok.extern.slf4j.Slf4j;
@@ -29,25 +34,69 @@ public class TelegramBotService extends TelegramLongPollingBot implements Initia
     @Autowired
     private OpenAiService openAiService;
 
-    private List<Message> messages = new ArrayList<>();
+    // Cria uma lista de mensagens da conversa
+    private List<Message> mensagens = new ArrayList<>();
 
     public TelegramBotService() {
-        // implementar este codigo
+        // Inicializa a lista de mensagens com a mensagem "system: prompt"
+        Message systemMessage = new Message("system", "Você deve responder sempre em formato json");
+        mensagens.add(systemMessage);
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        // implementar este codigo
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(this);
+        } catch (TelegramApiException e) {
+            log.error("Ocorreu um erro ao registrar o bot.", e);
+        }
     }
 
     @Override
     public void onUpdateReceived(Update update) {
-        // implementar este codigo
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            var mensagem = responder(update);
+            try {
+                execute(mensagem);
+            } catch (TelegramApiException e) {
+                log.error("Erro ao enviar a mensagem", e);
+            }
+        }
     }
 
-    private SendMessage responder(Update update) {
-        // implementar este codigo
-        return null;
+   private SendMessage responder(Update update) {
+        // Obtém a mensagem recebida e o chat ID
+        String msgRecebida = update.getMessage().getText().toLowerCase();
+        String chatId = update.getMessage().getChatId().toString();
+
+        // Registra a mensagem recebida no log
+        log.info("mensagem recebida -> {}", msgRecebida);
+
+        // Cria a mensagem do usuário e a adiciona à lista de mensagens
+        Message userMessage = new Message("user", msgRecebida);
+        mensagens.add(userMessage);
+
+        // Chama o chat GPT com a lista de mensagens
+        ChatGPTResponse gptResponse = openAiService.chat(mensagens);
+
+        // Obtém a última mensagem do GPT(resposta)
+        Choice ultimaMsgGPT = gptResponse.getChoices().get(gptResponse.getChoices().size() - 1);
+
+        // Adiciona a mensagem do GPT à lista de mensagens da interação
+        mensagens.add(ultimaMsgGPT.getMessage());
+
+        // Obtém a resposta do GPT
+        String resposta = ultimaMsgGPT.getMessage().getContent();
+
+        // Registra a mensagem enviada no log
+        log.info("mensagem enviada -> {}", resposta);
+
+        // Cria e retorna a mensagem de resposta
+        return SendMessage.builder()
+                .text(resposta)
+                .chatId(chatId)
+                .build();
     }
 
     @Override
